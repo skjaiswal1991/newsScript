2354d23caae84256dbc708a68debc08bbe20565d
rateusonline <106961091+rateusonline@users.noreply.github.com>
2023-02-02 20:43:30 +0530

    add yml file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
-#crontab -l | { cat; echo "@reboot pm2 start npm --name "Project-Name" -- start"; } | crontab -
+#crontab -l | { cat; echo "@reboot pm2 start npm --name "newsScript" -- start"; } | crontab -
 #give permissions 777
-sudo chmod -R 777 /var/www/html/Project-Name
+sudo chmod -R 777 /var/www/html/newsScript
 #install dependecies
-cd /var/www/html/Project-Name/ && npm install
+cd /var/www/html/newsScript/ && npm install
 #Check server running or not accordingly start or reload server
-pm2 describe Project-Name > /dev/null
+pm2 describe newsScript > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start npm --name "Project-Name" -- start
+  pm2 start npm --name "newsScript" -- start
 else
-  pm2 reload Project-Name
+  pm2 reload newsScript
 fi;
 
 #save pm2
